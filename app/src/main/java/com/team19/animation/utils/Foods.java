b82b587e7c4e2c792efd09fd326ddb958e3a9925
Crucialjun <nich.otieno@gmail.com>
2019-10-18 23:24:47 +0300

    Main Dishes

--- app/src/main/java/com/team19/animation/utils/Foods.java
+++ app/src/main/java/com/team19/animation/utils/Foods.java
@@ -4,27 +4,84 @@ import com.team19.animation.R;
 import com.team19.animation.models.Food;
 
 public class Foods {
-    public Food[] foods = {
+
+    public Food[] mainDishesFoods = {
             //Main_dishes
             Mustard_stuffed_chicken, Spicy_root_and_lentil_casserole, Chicken_biryani, chicken_chasseur
             , Oven_baked_risotto, Veggie_shepherds_pie, Hearty_pasta_soup, Crispy_Greek_style_pie
             , paella, salmon_with_roast_asparagus, Parmesan_spring_chicken
 
-            //Desserts
-            ,Profiterolesalted_toffee_ice_cream_sandwiches,Ultimate_lemon_meringue_pie
-            ,Matcha_mousse_cake,No_cook_chocolate_tart,Strawberry_cheesecake,Almond_and_apple_tart
-            ,Lavender_poached_pear_with_Poire_Williams_pudding,Pear_and_hazelnut_dartois
-            ,Ultimate_crème_brûlée,Champagne_raspberry_possets,Chocolate_hazelnut_ice_cream_cheesecake
-            ,Strawberry_and_elderflower_trifle,Luscious_lemon_baked_cheesecake
-
-            //Drinks
-            ,Old_Fashioned,Margarita,Cosmopolitan,Negroni,Moscow_Mule,Mojito,Whiskey_sour,Manhattan
-            ,Mimosa,Gimlet
     };
 
-    public static Food Mustard_stuffed_chicken = new Food("Mustard-stuffed chicken",getString(R.string.mustard_description),3340,"Main Meal", R.drawable.mustard);
-    public static Food Spicy_root_and_lentil_casserole = new Food("Spicy root & lentil casserole",getString(R.string.spicy_description),4675,"Main Meal", R.drawable.spicyroot);
-    public static Food Chicken_biryani = new Food("Chicken biryani",getString(R.string.biryani_description),3380,"Main Meal", R.drawable.chickenbiryani);
-    public static Food chicken_chasseur = new Food("One-pot chicken chasseur",getString(R.string.chicken_chasseur_description),2098,"Main Meal", R.drawable.onepot);
+    public static Food Mustard_stuffed_chicken = new Food("Mustard-stuffed chicken"
+            ,"This is so good we'd be surprised if this chicken fillet recipe doesn't become a firm favourite. Save it to your My Good Food collection and enjoy"
+            ,3340
+            ,"Main Meal"
+            , R.drawable.mustard);
+    public static Food Spicy_root_and_lentil_casserole = new Food("Spicy root & lentil casserole"
+            ,"The potatoes in this recipe take on the spicy flavours beautifully - our idea of the perfect veggie supper"
+            ,4675
+            ,"Main Meal"
+            , R.drawable.spicyroot);
+    public static Food Chicken_biryani = new Food("Chicken biryani"
+            ,"A great one-pot rice dish that can still be served up a few days later, perfect for leftovers"
+            ,3380
+            ,"Main Meal"
+            , R.drawable.chickenbiryani);
+    public static Food chicken_chasseur = new Food("One-pot chicken chasseur"
+            ,"This French bistro classic is easy to make at home and fabulous with creamy mash or crusty bread"
+            ,2098
+            ,"Main Meal"
+            , R.drawable.onepot);
+    public static Food Oven_baked_risotto = new Food("Oven-baked risotto"
+            ,"Cook this simple storecupboard risotto in the oven while you get on with something else – the result is still wonderfully creamy"
+            ,215
+            ,"Main Meal"
+            , R.drawable.ovenbaked);
+    public static Food Veggie_shepherds_pie = new Food("Veggie shepherd's pie with sweet potato mash"
+            ,"The secret to this shepherd's pie’s filling is to choose big carrots so they don’t lose their texture when cooked.."
+            ,1926
+            ,"Main Meal"
+            , R.drawable.veggie);
+
+    public static Food Hearty_pasta_soup = new Food("Hearty pasta soup"
+            ,"Do something different with a pack of tortellini. This filling soup is full of fibre, low fat and full of veg. The perfect lunch or supper"
+            ,1598
+            ,"Main Meal"
+            , R.drawable.pastasoup);
+
+    public static Food Crispy_Greek_style_pie = new Food("Crispy Greek-style pie"
+            ,"A crispy pie that you can adapt for your needs, add chicken or keep it veggie. A good fail-safe for your repertoire"
+            ,3344
+            ,"Main Meal"
+            , R.drawable.crispypie);
+    public static Food paella = new Food("Easiest ever paella"
+            ,"Think paella's too much of a challenge? Think again, this easy recipe makes it foolproof and is full of fantastic flavour..."
+            ,4089
+            ,"Main Meal"
+            , R.drawable.paella);
+    public static Food salmon_with_roast_asparagus = new Food("One-pan salmon with roast asparagus"
+            ,"For an easy side dish to complement a spring roast, just cook this recipe without the salmon"
+            ,276
+            ,"Main Meal"
+            , R.drawable.lasagne);
+    public static Food Parmesan_spring_chicken = new Food("Parmesan spring chicken"
+            ,"Dish up an easy, delicious chicken dinner that's full of spring flavours. The parmesan coating gives a satisfying crunch, and the meat inside stays tender..."
+            ,2503
+            ,"Main Meal"
+            , R.drawable.chickenbiryani);
+
+
+//    //Desserts
+//            ,Profiterolesalted_toffee_ice_cream_sandwiches,Ultimate_lemon_meringue_pie
+//            ,Matcha_mousse_cake,No_cook_chocolate_tart,Strawberry_cheesecake,Almond_and_apple_tart
+//            ,Lavender_poached_pear_with_Poire_Williams_pudding,Pear_and_hazelnut_dartois
+//            ,Ultimate_crème_brûlée,Champagne_raspberry_possets,Chocolate_hazelnut_ice_cream_cheesecake
+//            ,Strawberry_and_elderflower_trifle,Luscious_lemon_baked_cheesecake
+//
+//    //Drinks
+//            ,Old_Fashioned,Margarita,Cosmopolitan,Negroni,Moscow_Mule,Mojito,Whiskey_sour,Manhattan
+//            ,Mimosa,Gimlet
+
 
 }
