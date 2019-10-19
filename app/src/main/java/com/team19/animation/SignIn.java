f508a049472d3123c0fa5fdfd25c6590c4a31e02
Phil <kimathi.phil@gmail.com>
2019-10-19 15:06:32 +0300

    Added SignIn & SignUp

---
+++ app/src/main/java/com/team19/animation/SignIn.java
@@ -0,0 +1,25 @@
+package com.team19.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OpenSignupPage(View view) {
+        startActivity(new Intent(SignIn.this, SignUp.class));
+    }
+}
